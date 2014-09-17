b53be10897bfbf018b7ecad45dccf30e5094a3ee
Eftakhairul Islam <eftakhaiurl@gmial.com>
2014-09-17 16:04:21 -0400

    some modification lib/gisbn.rb file

--- lib/gisbn.rb
+++ lib/gisbn.rb
@@ -1,5 +1,18 @@
+#require all files
 require "gisbn/version"
+require 'rubygems'
+require 'open-uri'
+require 'json'
 
 module Gisbn
-  # Your code goes here...
+
+  @base_ur = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDKepjfaVBRcgsnPALw5s2UNyfOk-1FHUU&country=bd&q=isbn:'
+
+  def initialize(key, country = nil)
+    if !key.nil?
+      @base_ur ||= @base_ur + "/key=#{key}"
+    end
+
+  end
+
 end
