1ecffe856bafaf7f18b89922d8905f9680f79ff5
Eftakhairul Islam <eftakhairul@gmail.com>
2015-01-26 01:16:32 -0500

    basic workable version is done

--- gisbn.gemspec
+++ gisbn.gemspec
@@ -13,6 +13,8 @@ Gem::Specification.new do |spec|
   spec.homepage      = "https://github.com/eftakhairul/gisbn"
   spec.license       = "MIT"
 
+  spec.add_dependency "httparty"
+
   spec.files         = `git ls-files -z`.split("\x0")
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
--- lib/gisbn.rb
+++ lib/gisbn.rb
@@ -1,19 +1,28 @@
 #require all files
 require "gisbn/version"
 require 'rubygems'
-require 'open-uri'
+require 'net/http'
 require 'json'
+
 
 module Gisbn
 
   class Book
+    attr_reader :result, :isbn
     BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'
 
-    def initialize(number  = nil,
-                   key     = 'AIzaSyDKepjfaVBRcgsnPALw5s2UNyfOk-1FHUU',
-                   country = 'ca')
+    def initialize(isbn     = nil,
+                   key      = 'AIzaSyDKepjfaVBRcgsnPALw5s2UNyfOk-1FHUU',
+                   country  = 'ca')
 
-      @base_url = BASE_URL + "#{number}&key=#{key}&country=#{country}"
+
+      @isbn     = isbn
+      response = Net::HTTP.get_response(URI.parse(BASE_URL + "#{isbn}&key=#{key}&country=#{country}")).body
+      @result = JSON.parse(response)
+    end
+
+    def description
+      @result["items"]["description"]
     end
   end
 end
