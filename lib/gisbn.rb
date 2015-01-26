4ba963d93c056faf13ab051f08147113720d2c19
Eftakhairul Islam <eftakhaiurl@gmial.com>
2015-01-26 11:41:02 -0500

    Core file of GEM is updated

--- lib/gisbn.rb
+++ lib/gisbn.rb
@@ -17,12 +17,28 @@ module Gisbn
 
 
       @isbn     = isbn
-      response = Net::HTTP.get_response(URI.parse(BASE_URL + "#{isbn}&key=#{key}&country=#{country}")).body
-      @result = JSON.parse(response)
+      response  = Net::HTTP.get_response(URI.parse(BASE_URL + "#{isbn}&key=#{key}&country=#{country}")).body
+      @result   = JSON.parse(response)
     end
 
     def description
       @result["items"]["description"]
+    end
+
+    def title
+      @result["items"]["volumeInfo"]["title"]
+    end
+
+    def authors
+      @result["items"]["volumeInfo"]["authors"].join(",")
+    end
+
+    def authors_as_array
+      @result["items"]["volumeInfo"]["authors"]
+    end
+
+    def ISBN_10
+      @result["items"]["industryIdentifiers"][""]
     end
   end
 end
