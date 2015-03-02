07436fca5d56393f21ee51c357c290a3c953d03d
Eftakhairul Islam <eftakhairul@gmail.com>
2015-03-01 19:53:30 -0500

    rspec worked perfectly

--- spec/gisbn_spec.rb
+++ spec/gisbn_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
-describe GISBN do
-  it 'does stuff' do
+describe "gisbn" do
+  subject { Gisbn::Book.new "0262033844" }
 
+  describe '#process' do
+    let(:output) { "Introduction to Algorithms" }
+
+    it 'book title' do
+      expect(output.downcase).to eq subject.title.downcase
+    end
   end
 end
